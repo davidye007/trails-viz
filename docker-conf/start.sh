880aa39454b6d4ae9f5908ea9237764fe1d5d0e8
davidye007 <davidyeap@outlook.com>
2024-10-06 13:32:33 -0700

    add logging for uwsgi and make start.sh processes run in background

--- docker-conf/start.sh
+++ docker-conf/start.sh
@@ -4,28 +4,8 @@
 pkill nginx || echo "nginx not running"
 pkill uwsgi || echo "uWSGI not running"
 
-# Start uWSGI
-/home/nginx/miniconda3/envs/trails-viz-api/bin/uwsgi --ini /home/nginx/trails-viz/trails-viz-api/uwsgi-config.ini &
+# Start uWSGI in background
+/home/nginx/miniconda3/envs/trails-viz-api/bin/uwsgi --ini /home/nginx/trails-viz/trails-viz-api/uwsgi-config.ini
 
-# Capture the PID of the uWSGI process
-UWSGI_PID=$!
-
-# Start nginx
-nginx -c /home/nginx/trails-viz/docker-conf/nginx.conf -g 'daemon off;' &
-
-# Capture the PID of the nginx process
-NGINX_PID=$!
-
-# Function to handle termination signals
-cleanup() {
-    echo "Stopping services..."
-    kill -TERM "$UWSGI_PID" "$NGINX_PID"
-    wait "$UWSGI_PID" "$NGINX_PID"
-    echo "Services stopped."
-}
-
-# Trap termination signals
-trap cleanup SIGINT SIGTERM
-
-# Wait for the services to exit
-wait "$UWSGI_PID" "$NGINX_PID"
+# Start nginx in background
+nginx -c /home/nginx/trails-viz/docker-conf/nginx.conf
